f36b6621f37bf6aa6706b9fdee6ee5e00e216bf2
paupaugoz <37976584+paupaugoz@users.noreply.github.com>
2019-05-26 13:04:11 +0800

    Upload bg

--- src/components/introduction.jsx
+++ src/components/introduction.jsx
@@ -3,15 +3,15 @@ import React, { Component } from 'react'
 export default class Introduction extends Component {
   render () {
     return (
-      <div className="intro-wrapper">
+      <div id="introduction" className="intro-wrapper">
         <div className="flex-wrapper">
-          <h1 className="animated fadeInLeft">Hi I'm Gozum</h1>
-          <h1 className="animated fadeInRight">I am a front-end web developer.</h1>
-          <h1 className="animated bounce">Test</h1>
+          <h1 className="animated fadeInDownBig blue">Hi I'm Gozum</h1>
+          <h1 className="animated fadeInDown delay-1s pink">I am a front-end web developer.</h1>
+          <h1 className="animated fadeInDown delay-2s white">Test</h1>
+          <h1 className="animated fadeInDown delay-3s peach">Hi</h1>
         </div>
 
-      </div>
-     
+      </div>    
     )
   }
 }
